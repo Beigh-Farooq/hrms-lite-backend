691f62b52750e345acee27e36eba36fa9e593f49
Beigh-Farooq <beighfarooq199@gmail.com>
2026-01-30 15:49:48 +0530

    Implement employee CRUD APIs

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 from app.database import engine, Base 
 from app import models 
+from app.routers import employees
 
 app = FastAPI(title="HRMS Lite API")
 
 Base.metadata.create_all(bind=engine)
+
+app.include_router(employees.router)
 
 @app.get("/")
 def root():
