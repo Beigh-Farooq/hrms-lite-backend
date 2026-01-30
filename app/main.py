587c4b8613d3e1c695373a744a56020f36cc5272
Beigh-Farooq <beighfarooq199@gmail.com>
2026-01-30 16:01:40 +0530

    Implement attendance management APIs

--- app/main.py
+++ app/main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
 from app.database import engine, Base 
 from app import models 
-from app.routers import employees
+from app.routers import employees,attendance
 
 app = FastAPI(title="HRMS Lite API")
 
 Base.metadata.create_all(bind=engine)
 
 app.include_router(employees.router)
+app.include_router(attendance.router)
 
 @app.get("/")
 def root():
---
+++ app/routers/attendance.py
@@ -0,0 +1,76 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from app.database import get_db
+from app import models, schemas
+from datetime import date
+
+router = APIRouter(prefix="/attendance",tags=["Attendance"])
+
+
+# Mark Attendance API (POST /attendance)
+# Employee must exist
+# Only one attendance record per employee per date
+# Proper error messages
+@router.post("",status_code=status.HTTP_201_CREATED)
+def mark_attendance(attendance: schemas.AttendanceCreate, db: Session = Depends(get_db)):
+    # Check employee exists
+    employee = db.query(models.Employee).filter(
+        models.Employee.employee_id == attendance.employee_id
+    ).first()
+
+    if not employee:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Employee not found"
+        )
+
+    # Check duplicate attendance for same date
+    existing_record = db.query(models.Attendance).filter(
+        models.Attendance.employee_id == employee.id,
+        models.Attendance.date == attendance.date
+    ).first()
+
+    if existing_record:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Attendance already marked for this date"
+        )
+
+    new_attendance = models.Attendance(
+        employee_id=employee.id,
+        date=attendance.date,
+        status=attendance.status
+    )
+
+    db.add(new_attendance)
+    db.commit()
+
+    return {"message": "Attendance marked successfully"}
+
+
+
+
+
+
+
+# View Attendance API (GET /attendance/{employee_id})
+@router.get("/{employee_id}", response_model=schemas.EmployeeAttendanceResponse)
+def get_attendance(employee_id: str, db: Session = Depends(get_db) ):
+    employee = db.query(models.Employee).filter(
+        models.Employee.employee_id == employee_id
+    ).first()
+
+    if not employee:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Employee not found"
+        )
+
+    records = db.query(models.Attendance).filter(
+        models.Attendance.employee_id == employee.id
+    ).order_by(models.Attendance.date.desc()).all()
+
+    return {
+        "employee_id": employee.employee_id,
+        "records": records
+    }
