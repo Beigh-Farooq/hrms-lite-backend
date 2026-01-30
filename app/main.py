a7537c99050d79f629fd113b467e609cf2843675
Beigh-Farooq <beighfarooq199@gmail.com>
2026-01-30 16:33:45 +0530

    Enable CORS for frontend integration

--- app/main.py
+++ app/main.py
@@ -2,10 +2,24 @@ from fastapi import FastAPI
 from app.database import engine, Base 
 from app import models 
 from app.routers import employees,attendance
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="HRMS Lite API")
 
 Base.metadata.create_all(bind=engine)
+
+
+# CORS CONFIG
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(employees.router)
 app.include_router(attendance.router)
